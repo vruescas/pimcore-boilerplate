6ae5090dfa749357e5fd7434dfa16b06098d9694
Valentín Ruescas <var.vlc@coma.de>
2023-05-17 10:58:16 +0200

    [TASK] Install Empty Pimcore

---
+++ config/bundles.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle::class => ['all' => true],
+    Pimcore\Bundle\CustomReportsBundle\PimcoreCustomReportsBundle::class => ['all' => true],
+    Pimcore\Bundle\GlossaryBundle\PimcoreGlossaryBundle::class => ['all' => true],
+    Pimcore\Bundle\SeoBundle\PimcoreSeoBundle::class => ['all' => true],
+    Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
+    Pimcore\Bundle\StaticRoutesBundle\PimcoreStaticRoutesBundle::class => ['all' => true],
+    Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle::class => ['all' => true],
+    Pimcore\Bundle\UuidBundle\PimcoreUuidBundle::class => ['all' => true],
+    Pimcore\Bundle\WordExportBundle\PimcoreWordExportBundle::class => ['all' => true],
+    Pimcore\Bundle\XliffBundle\PimcoreXliffBundle::class => ['all' => true],
+];
